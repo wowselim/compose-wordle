566b3cea1e6a7f57b52328241ce901c088bc4a56
Gustav Karlsson <gustav.karlsson@gmail.com>
2022-02-08 22:41:11 +0100

    Support multiple files in zip

--- app/src/main/java/co/selim/wordle/words/WordRepository.kt
+++ app/src/main/java/co/selim/wordle/words/WordRepository.kt
@@ -3,6 +3,7 @@ package co.selim.wordle.words
 import android.content.Context
 import androidx.annotation.RawRes
 import co.selim.wordle.R
+import java.io.InputStream
 import java.util.zip.ZipInputStream
 
 class WordRepository(context: Context) {
@@ -10,18 +11,34 @@ class WordRepository(context: Context) {
     private val allWords by lazy { readWordsFromFile(context, R.raw.all_words) + riddleWords }
 
     private fun readWordsFromFile(context: Context, @RawRes riddleWords: Int): Set<String> {
-        return ZipInputStream(context.resources.openRawResource(riddleWords))
-            .use { stream ->
-                stream.also(ZipInputStream::getNextEntry)
-                    .bufferedReader()
+        return context.resources.openRawResource(riddleWords).useUnzipped { files ->
+            files.flatMap { stream ->
+                stream.bufferedReader()
                     .lineSequence()
                     .filter { it.isNotBlank() }
                     .map { it.trim().uppercase() }
-                    .toSet()
-            }
+            }.toSet()
+        }
     }
 
+    // TODO Make suspending
     fun getWord(length: Int): String = riddleWords.random()
 
+    // TODO Make suspending
     fun isValid(word: String): Boolean = word in allWords
+}
+
+private fun <T> InputStream.useUnzipped(block: (Sequence<InputStream>) -> T): T {
+    val sequence = sequence {
+        ZipInputStream(this@useUnzipped).use { zipStream ->
+            var nextEntry = zipStream.nextEntry
+            while (nextEntry != null) {
+                if (!nextEntry.isDirectory) {
+                    yield(zipStream)
+                }
+                nextEntry = zipStream.nextEntry
+            }
+        }
+    }
+    return block(sequence)
 }
