b5f8164be4ac09f424d5a4091797f0a761c33631
Gustav Karlsson <gustav.karlsson@gmail.com>
2022-02-08 21:59:15 +0100

    Read from zipped files

--- app/src/main/java/co/selim/wordle/words/WordRepository.kt
+++ app/src/main/java/co/selim/wordle/words/WordRepository.kt
@@ -3,18 +3,22 @@ package co.selim.wordle.words
 import android.content.Context
 import androidx.annotation.RawRes
 import co.selim.wordle.R
+import java.util.zip.ZipInputStream
 
 class WordRepository(context: Context) {
     private val riddleWords by lazy { readWordsFromFile(context, R.raw.riddle_words) }
     private val allWords by lazy { readWordsFromFile(context, R.raw.all_words) + riddleWords }
 
     private fun readWordsFromFile(context: Context, @RawRes riddleWords: Int): Set<String> {
-        return context.resources.openRawResource(riddleWords).reader().useLines { lines ->
-            lines
-                .filter { it.isNotBlank() }
-                .map { it.trim().uppercase() }
-                .toSet()
-        }
+        return ZipInputStream(context.resources.openRawResource(riddleWords))
+            .use { stream ->
+                stream.also(ZipInputStream::getNextEntry)
+                    .bufferedReader()
+                    .lineSequence()
+                    .filter { it.isNotBlank() }
+                    .map { it.trim().uppercase() }
+                    .toSet()
+            }
     }
 
     fun getWord(length: Int): String = riddleWords.random()
